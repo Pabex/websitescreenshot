e0dffe7c60b3a66ad8985a00cf2b0bbe39c3a76c
Fede Bertola <federicojbertola@gmail.com>
2020-04-24 17:37:28 -0300

    Add delay to screenshot class.

--- app/screenshot.py
+++ app/screenshot.py
@@ -5,8 +5,9 @@ from selenium.webdriver.chrome.options import Options
 
 
 class Screenshot:
-    def __init__(self, url):
+    def __init__(self, url, delay=1):
         self.url = url
+        self.delay = delay
         options = Options()
         options.add_argument("--headless")
         options.add_argument("--start-maximized")
@@ -14,7 +15,7 @@ class Screenshot:
 
     def save_image(self):
         self.driver.get(self.url)
-        time.sleep(1)
+        time.sleep(self.delay)
 
         self.driver.get_screenshot_as_file('screenshot_%s.png' % str(int(time.time())))
         self.driver.quit()
@@ -22,7 +23,7 @@ class Screenshot:
 
     def get_image(self):
         self.driver.get(self.url)
-        time.sleep(1)
+        time.sleep(self.delay)
         png = self.driver.get_screenshot_as_png()
         return png
 
