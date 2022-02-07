2e7cc3f7842a9ed07245b29fdd24d87f873c2bad
Fede Bertola <federicojbertola@gmail.com>
2022-02-07 19:59:11 -0300

    Add support to Facebook and Instagram screenshots.

--- app/screenshot.py
+++ app/screenshot.py
@@ -1,11 +1,15 @@
 import io
+import os
 import sys
 import time
 import imagehash
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from PIL import Image
-from urllib.parse import unquote
 
 
 class Screenshot:
@@ -23,7 +27,10 @@ class Screenshot:
         options.add_argument("--disable-browser-side-navigation")
         options.add_argument("--disable-gpu")
         options.add_argument('--dns-prefetch-disable')  # https://sqa.stackexchange.com/a/17955
-        options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36")
+        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36")
+        prefs = {"profile.default_content_setting_values.notifications": 2}
+        options.add_experimental_option("prefs", prefs)
+        options.add_argument("user-data-dir=data_chrome")
         self.driver = webdriver.Chrome('../chromedriver/chromedriver', options=options)
         self.driver.set_window_size(1280, 720)
         self.driver.set_page_load_timeout(30)
@@ -40,10 +47,16 @@ class Screenshot:
         return True
 
     def get_image(self):
+        png = None
         try:
-            self.driver.get(self.url)
-            time.sleep(self.delay)
-            png = self.driver.get_screenshot_as_png()
+            if "facebook.com" in self.url:
+                png = self.facebook()
+            elif "instagram.com" in self.url:
+                png = self.instagram()
+            else:
+                self.driver.get(self.url)
+                time.sleep(self.delay)
+                png = self.driver.get_screenshot_as_png()
             self.driver.quit()
             if self.__is_white_image(png):
                 return False
@@ -51,7 +64,70 @@ class Screenshot:
         except Exception as e:
             print("Error en %s: " % self.url, str(e))
             self.driver.quit()
-        return False
+        return png
+
+    def facebook(self):
+        self.driver.set_page_load_timeout(60)
+        self.driver.get(self.url)
+        time.sleep(6)
+
+        def login_facebook():
+            txt_email = self.driver.find_element(By.ID, "email")
+            txt_password = self.driver.find_element(By.ID, "pass")
+            email = os.environ.get("EMAIL_FACEBOOK")
+            password = os.environ.get("PASSWORD_FACEBOOK")
+            time.sleep(4)
+            txt_email.send_keys(email)
+            txt_password.send_keys(password)
+            txt_password.send_keys(Keys.ENTER)
+            time.sleep(2)
+            try:
+                WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[href='/']")))
+            except:
+                pass
+
+        if len(self.driver.find_elements(By.ID, "email")) > 0 and len(self.driver.find_elements(By.ID, "pass")) > 0:
+            # Esta en el login, tenemos que iniciar sesion.
+            login_facebook()
+        elif len(self.driver.find_elements(By.CSS_SELECTOR, "div[aria-label='No iniciaste sesión'")) > 0:
+            self.driver.get("https://www.facebook.com")
+            WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.ID, "email")))
+            login_facebook()
+            self.driver.get(self.url)
+        elif "Escribe tu contraseña" in self.driver.page_source:
+            time.sleep(3)
+            txt_password = self.driver.find_element(By.ID, "pass")
+            password = os.environ.get("PASSWORD_FACEBOOK")
+            txt_password.send_keys(password)
+            txt_password.send_keys(Keys.ENTER)
+            time.sleep(3)
+
+        png = self.driver.get_screenshot_as_png()
+        return png
+
+    def instagram(self):
+        self.driver.set_page_load_timeout(60)
+        self.driver.get(self.url)
+        time.sleep(6)
+
+        email_instagram = os.environ.get("EMAIL_INSTAGRAM")
+        password_instagram = os.environ.get("PASSWORD_INSTAGRAM")
+
+        if len(self.driver.find_elements(By.CSS_SELECTOR, "input[name='username']")) > 0 and len(self.driver.find_elements(By.CSS_SELECTOR, "input[name='password']")):
+            # Esta en el login, tenemos que iniciar sesion.
+            txt_username = self.driver.find_element(By.CSS_SELECTOR, "input[name='username']")
+            txt_password = self.driver.find_element(By.CSS_SELECTOR, "input[name='password']")
+            time.sleep(4)
+            txt_username.send_keys(email_instagram)
+            txt_password.send_keys(password_instagram)
+            txt_password.send_keys(Keys.ENTER)
+            time.sleep(4)
+
+        WebDriverWait(self.driver, 20, ignored_exceptions=True).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "img[data-testid='user-avatar']")))
+
+        png = self.driver.get_screenshot_as_png()
+
+        return png
 
     def __is_white_image(self, bytes_image) -> bool:
         is_white = False
